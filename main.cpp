f219b27837848a369149bdb0e073b23feaac7786
RenzDureza <durezarenz@gmail.com>
2026-03-12 10:51:50 +0800

    Add expression evaluation and error handling

--- main.cpp
+++ main.cpp
@@ -8,17 +8,22 @@
 #include <utility>
 #include <vector>
 #include <unordered_map>
+#include <stack>
+#include <string>
+#include <cctype>
 using namespace std;
 
 vector<pair<regex, string>> patterns = {
-	{regex(R"(^\b(var|input|output)\b)"), "Keyword"},
-	{regex(R"(^[0-9][0-9]*)"), "Integer"},
+	{regex(R"(^(var|input|output))"), "Keyword"},
+	{regex(R"([0-9][0-9]*)"), "Integer"},
 	{regex(R"(^[_a-zA-Z][_a-zA-Z0-9]*)"), "Identifier"},
 	{regex(R"(^//[^\n]*)"), "Comment"},
-	{regex(R"(^[{}\[\](),.:])"), "Punctuation"},
-	{regex(R"(^[;])"), "Terminator"},
-	{regex(R"(^[-+*/=]+)"), "Operator"},
-	{regex(R"(^\s+)"), "Whitespace"}};
+	// {regex(R"(^[{}\[\](),.:])"), "Punctuation"},
+	{regex(R"(;)"), "Terminator"},
+	{regex(R"([-+*/()])"), "Operator"},
+	{regex(R"(=)"), "Assignment"},
+	{regex(R"(^\s+)"), "Whitespace"},
+};
 
 regex r_initialization("var\\s+([a-zA-Z_][a-zA-Z0-9_]*)\\s*=\\s*[0-9]");
 // regex
@@ -42,21 +47,26 @@ regex r_initialization("var\\s+([a-zA-Z_][a-zA-Z0-9_]*)\\s*=\\s*[0-9]");
 // 	}
 // }
 
+vector<vector<pair<string, string>>> token_lines;
+vector<pair<string, string>> line;
+unordered_map<string, int> variable_table;
+
 bool isInputValid(string input);
+bool varExists(string var_name);
+
+bool isOperator(char c);
+int precedence(char op);
+double applyOperation(double a, double b, char op);
+double evaluateExpression(const string& expression);
 
 int main() {
-	vector<vector<pair<string, string>>> token_lines;
-	vector<pair<string, string>> line;
-	unordered_map<string, int> variable_table;
 	ifstream codeFile("main.txt");
 	stringstream buffer;
 	buffer << codeFile.rdbuf();
 	string myCode = buffer.str();
 
-	cout << "Source Code:" << endl;
-	cout << "==========================" << endl;
+	cout << "==========================" << " Source Code:" << endl;
 	cout << myCode << endl;
-	cout << "==========================" << endl;
 
 	size_t pos = 0;
 	while (pos < myCode.size()) {
@@ -87,52 +97,93 @@ int main() {
 	}
 
 	size_t l_count = 1;
+	cout << "==========================" << " Tokenizing.." << endl;
 	for (const auto &line : token_lines) {
 		cout << "Line " << l_count << ": " << endl;
 		for (const auto &pair : line) {
 			cout << pair.first << " - " << pair.second << endl;
 		}
+		cout << endl;
+		l_count++;
+	}
+
+	l_count = 1;
+	cout << "==========================" << " Parsing.." << endl;
+	for (const auto &line : token_lines) {
+		for (const auto &pair : line) {
+		}
 		if (line[0].first == "var") {
 			if (line[1].second == "Identifier" && line[2].second == "Terminator") { // initialization
 				variable_table.insert({line[1].first, NULL});
 			}
-			else if (line[1].second == "Identifier" && line[2].second == "Operator" && line[3].second == "Integer" && line[4].second == "Terminator") { // initialization with assignment
+			else if (line[1].second == "Identifier" && line[2].second == "Assignment" && line[3].second == "Integer" && line[4].second == "Terminator") { // initialization with assignment
 				variable_table.insert({line[1].first, stoi(line[3].first)});
+			}
+			else if (line[1].second == "Identifier" && line[2].second == "Assignment" && line[4].second != "Terminator"){ // initialization with expression assignment
+				string expression;
+				for (size_t i = 3; i < line.size() - 1; i++) {
+					if (line[i].second == "Integer" || line[i].second == "Operator") {
+						expression += line[i].first + " ";
+					}
+					else if (line[i].second == "Identifier" && varExists(line[i].first)){
+						expression += to_string(variable_table[line[i].first]) + " ";
+					}
+					else if (line[i].second == "Identifier" && !varExists(line[i].first)){
+						cout << "Identifier " << line[i].first << " was not declared first." << endl;
+						break;
+					}
+					else {
+						cout << "Invalid use of " << line[i].first << " at Line " << l_count << endl;
+					}
+				}
+				cout << "Expression: " << expression << endl;
+				variable_table.insert({line[1].first, evaluateExpression(expression)});
+			}
+			else if (line[-2].second == "Operator" && line[-1].second == "Terminator") {
+				cout << "Missing identifier or integer at " << line[-2].second << endl;
 			}
 			else {
 				cout << "Out of Scope var use" << endl;
 			}
-		} else if (line[0].first == "input") { // input from console
+		} 
+		else if (line[0].first == "input") { // input from console
 			if (line[1].second == "Identifier" && line[2].second == "Terminator") {
-				string input;
-				cout << line[1].first << ": ";
-				cin >> input;
+				if (varExists(line[1].first)){
+					string input;
+					cout << line[1].first << ": ";
+					cin >> input;
 
-				if (isInputValid) variable_table.insert({line[1].first, stoi(input)});
-				else cout << "Invalid input operation" << endl;
+					if (isInputValid) variable_table[line[1].first] = stoi(input);
+					else cout << "Invalid input operation" << endl;
+				}
+				else cout << "Identifier " << line[1].first << " was not declared first." << endl;				
 			}
 			else {
 				cout << "Out of Scope input use" << endl;
 			}
-		} else if (line[0].first == "output") {
-			if (line[1].second == "Identifier" && line[2].second == "Terminator" && (variable_table.find(line[1].first) != variable_table.end())) {
-				cout << line[1].first << ": " << variable_table[line[1].first] << endl;
-			}
-			else if ((variable_table.find(line[1].first) == variable_table.end())){
-				cout << "Undefined variable: " << line[1].first << endl;
+		} 
+		else if (line[0].first == "output") {
+			if (line[1].second == "Identifier" && line[2].second == "Terminator") {
+				if (varExists(line[1].first)) cout << line[1].first << ": " << variable_table[line[1].first] << endl;
+				else cout << "Undefined variable: " << line[1].first << endl;
 			}
 			else {
 				cout << "Out of Scope output use" << endl;
 			}
-		} else {
-			cout << "Incorrect Keyword at " << line[0].first << endl;
+		} 
+		else {
+			cout << "Invalid use of " << line[0].first << " at Line " << l_count << endl;
 			break;
 		}
 		l_count++;
-		cout << "==========================" << endl;
 	}
 
-	cout << "Variable Table: " << endl;
+	// string abc = "1 + 2 * ( 10 / 5 )";
+	// double result = evaluateExpression(abc);
+	// cout << "Result: " << result << endl;
+	// variable_table.insert({"abc", result});
+
+	cout << "==========================" << " Variable Table: " << endl;
 	for (const auto &var : variable_table) {
 	cout << var.first << " - " << var.second << endl;
 	}
@@ -143,4 +194,81 @@ int main() {
 //TODO
 bool isInputValid(string input) {
 	return true;
+}
+
+bool varExists(string var_name){
+	if (variable_table.find(var_name) != variable_table.end()) return true;
+	else return false;
+}
+
+bool isOperator(char c){
+	return c =='+' || c =='-' || c =='*' || c =='/' || c =='^';
+}
+int precedence(char op){
+	if (op =='+' || op =='-') return 1;
+	if (op =='*' || op == '/') return 2;
+	if (op == '^') return 3;
+	return 0;
+}
+double applyOperation(double a, double b, char op){
+	switch(op){
+		case '+': return a + b;
+		case '-': return a - b;
+		case '*': return a * b;
+		case '/': return a / b;
+		case '^': return pow(a,b);
+		default: return 0;
+	}
+}
+double evaluateExpression(const string& expression){
+	stack<char> operators;
+	stack<double> operands;
+	stringstream ss(expression);
+
+	string token;
+	while (getline(ss, token, ' ')){
+		if (token.empty()) continue;
+		if (isdigit(token[0])){
+			double num;
+			stringstream(token) >> num;
+			operands.push(num);
+		}
+		else if(isOperator(token[0])){
+			char op = token[0];
+			while (!operators.empty() && precedence(operators.top()) >= precedence(op)){
+				double b = operands.top();
+				operands.pop();
+				double a = operands.top();
+				operands.pop();
+				char op2 = operators.top();
+				operators.pop();
+				operands.push(applyOperation(a, b, op));
+			}
+			operators.push(op);
+		}
+		else if(token[0] == '(') {
+			operators.push('(');
+		}else if(token[0] == ')') {
+			while (!operators.empty() && operators.top() != '('){
+				double b = operands.top();
+				operands.pop();
+				double a = operands.top();
+				operands.pop();
+				char op = operators.top();
+				operators.pop();
+				operands.push(applyOperation(a, b, op));
+			}
+			operators.pop();
+		}
+	}
+	while (!operators.empty()){
+		double b = operands.top();
+		operands.pop();
+		double a = operands.top();
+		operands.pop();
+		char op = operators.top();
+		operators.pop();
+		operands.push(applyOperation(a, b, op));
+	}
+	return operands.top();
 }
